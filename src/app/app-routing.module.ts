b606c244bc698d6be8ab7db34d7a7323a0857256
Rhama Krisner <rhama.krisner@gmail.com>
2024-08-18 23:33:36 -0300

    feat: Commit das alterações incluindo modificações, exclusões e novos arquivos na aplicação.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlunoListComponent } from './entities/aluno/aluno-list/aluno-list.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'aluno-list', component: AlunoListComponent },
+    { path: 'home', component: HomeComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { AppComponent } from '../app.component';
+import { AppComponent } from '../../app.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenu } from "@angular/material/menu";
 import { MatMenuModule } from '@angular/material/menu';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -21,5 +22,11 @@ import { MatMenuModule } from '@angular/material/menu';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+    constructor(private router:Router){
 
+    }
+
+    navigate(route:String){
+        this.router.navigate([route])
+    }
 }
